b20e2d29abdf66ceefd41746ff5ca4abc5880eb4
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-13 18:47:02 +0100

    fatto la logica con il bonus senca gioco e grafica

---
+++ script.js
@@ -0,0 +1,25 @@
+let emialinsert = prompt("La tua e-mail e:");
+
+let arraymail = ["matteo@gmail.com","fabio@gmail.com","pietro@gmail.com","anna@gmail.com"];
+let flag = 0;
+let emailout;
+for(let i=0;i<arraymail.length;i++){
+    if(emialinsert == arraymail[i]){
+        flag = 1;
+        emailout= arraymail[i]
+    }
+}
+if(flag){
+    console.log(`La email inserita e: ${emailout} `);
+}else{
+    console.log(`La email inscerita non che in lista se vuoi aggungere dentro la lista?`);
+    let metti = prompt("1--si , 0--no");
+    if(metti){
+        arraymail[(arraymail.length)]=emialinsert;
+        console.log(`Nuova lista e-mail accetabile e: `);
+        for(let i = 0 ; i<arraymail.length;i++)
+            console.log(arraymail[i]);
+        
+    }
+
+}
