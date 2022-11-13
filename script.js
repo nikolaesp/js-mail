e8845eb34726adde29daccb0be1e20af875c34e6
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-13 20:27:29 +0100

    gioco dadi

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-let emialinsert = prompt("La tua e-mail e:");
+let emialinsert = prompt("La tua e-mail e:"); //inserisci email
 
-let arraymail = ["matteo@gmail.com","fabio@gmail.com","pietro@gmail.com","anna@gmail.com"];
+let arraymail = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"]; // inizializa lista
 let flag = 0;
 let emailout;
-for(let i=0;i<arraymail.length;i++){
-    if(emialinsert == arraymail[i]){
+for (let i = 0; i < arraymail.length; i++) {
+    if (emialinsert == arraymail[i]) {//vedi se email inserito che o non che in lista
         flag = 1;
-        emailout= arraymail[i]
+        emailout = arraymail[i]
     }
 }
-if(flag){
+if (flag) {
     console.log(`La email inserita e: ${emailout} `);
-}else{
+} else {
     console.log(`La email inscerita non che in lista se vuoi aggungere dentro la lista?`);
     let metti = prompt("1--si , 0--no");
-    if(metti){
-        arraymail[(arraymail.length)]=emialinsert;
+    if (metti) {
+        arraymail[(arraymail.length)] = emialinsert;//metti un altro elemento con la nostra email lista
     }
 
 }
 console.log(`Lista e-mail accetabile e: `);
-for(let i = 0 ; i<arraymail.length;i++)
-    console.log(arraymail[i]);
+for (let i = 0; i < arraymail.length; i++)
+    console.log(arraymail[i]); //stampa la lista 
+console.log(`-------------------------------------------------------
+-----------Gioco dei dadi con random numeri------------`)
+let computer = Math.floor((Math.random() * 6) + 1);
+let user = Math.floor((Math.random() * 6) + 1);
+if (computer == user) {
+    console.log(`Sonno iguale vinto nessuno`);
+}
+else {
+    if (computer > user)
+        console.log(`Vinto Computer C:${computer} U:${user}`);
+    else
+        console.log(`Vinto Utente C:${computer} U:${user}`);
+}
